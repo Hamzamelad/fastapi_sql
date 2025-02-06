cf4e74886b294dee91553a29ab535145a7556f64
Hamzamelad <hamza99melad@gmail.com>
2025-02-06 16:15:38 +0200

    Inincial commit.

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "sqlite:///./users.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = "postgresql://postgres:admin@localhost:5432/fastapi_db"
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
---
+++ passenger_wsgi.py
@@ -0,0 +1,5 @@
+from main import app  # Replace 'main' with your actual script name
+from fastapi.middleware.wsgi import WSGIMiddleware
+
+# Wrap FastAPI app with WSGI middleware
+app = WSGIMiddleware(app)
